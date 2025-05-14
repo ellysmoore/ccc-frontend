cf316cc33dbf7f355edbab2cc9f7e1e20589820e
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2025-05-14 14:04:46 +0100

    commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,14 +10,17 @@ const NotFoundPage = () => {
 
   return (
     <Suspense fallback={null}>
-      {error && (
-        <div className="container mx-auto py-10">
-          <div className="flex flex-col items-center justify-center text-center space-y-6">
-            <h1 className="text-3xl font-bold text-primary">Page Not Found</h1>
-            <Button href="/" label="GO HOME" containerClassName="!w-fit" />
-          </div>
+      <div className="container mx-auto py-10">
+        <div className="flex flex-col items-center justify-center text-center space-y-6">
+          <h1 className="text-3xl font-bold text-primary">Page Not Found</h1>
+          {error && (
+            <p className="text-red-600 text-sm">
+              Reason: <span className="font-medium">{error}</span>
+            </p>
+          )}
+          <Button href="/" label="GO HOME" containerClassName="!w-fit" />
         </div>
-      )}
+      </div>
     </Suspense>
   );
 };
