c97c156c437c6660b6980642f14d5e4d29e3681f
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2025-05-14 11:54:08 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Add more config options below as needed
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
