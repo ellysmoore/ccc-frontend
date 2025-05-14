93afb32b1f341c1162c61f3b6256835645b7ce32
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2025-05-14 14:13:32 +0100

    commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,27 +1,14 @@
 "use client";
 
-import { Button } from "@/components/Button";
 import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
+import NotFoundContent from "./NotFoundContent";
 
 const NotFoundPage = () => {
-  const searchParams = useSearchParams();
-  const error = searchParams.get("error");
 
   return (
     <Suspense fallback={null}>
-      <div className="container mx-auto py-10">
-        <div className="flex flex-col items-center justify-center text-center space-y-6">
-          <h1 className="text-3xl font-bold text-primary">Page Not Found</h1>
-          {error && (
-            <p className="text-red-600 text-sm">
-              Reason: <span className="font-medium">{error}</span>
-            </p>
-          )}
-          <Button href="/" label="GO HOME" containerClassName="!w-fit" />
-        </div>
-      </div>
-    </Suspense>
+    <NotFoundContent />
+  </Suspense>
   );
 };
 
