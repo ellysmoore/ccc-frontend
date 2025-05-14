a9d3d52bd7f75da41ccebad50eccb23ce8b79ae2
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2025-05-14 12:41:07 +0100

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,22 @@
 
 import { Button } from "@/components/Button";
 import React, { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
 const NotFoundPage = () => {
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+
   return (
     <Suspense fallback={null}>
-      <div className="container mx-auto py-10">
-        <div className="flex flex-col items-center justify-center text-center space-y-6">
-          <h1 className="text-3xl font-bold text-primary">Page Not Found</h1>
-          <Button href="/" label="GO HOME" containerClassName="!w-fit" />
+      {error && (
+        <div className="container mx-auto py-10">
+          <div className="flex flex-col items-center justify-center text-center space-y-6">
+            <h1 className="text-3xl font-bold text-primary">Page Not Found</h1>
+            <Button href="/" label="GO HOME" containerClassName="!w-fit" />
+          </div>
         </div>
-      </div>
+      )}
     </Suspense>
   );
 };
