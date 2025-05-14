0964fefbf3140d7969eb2209d6bd724632f90329
Tonye Greene <35413072+ellysmoore@users.noreply.github.com>
2025-05-14 11:42:17 +0100

    commit

--- src/app/(main)/categories/[id]/page.tsx
+++ src/app/(main)/categories/[id]/page.tsx
@@ -6,7 +6,22 @@ import { CategoryMainSection } from '../(components)/CategoryMainSection'
 import { CATEGORIES, MESSAGE_DATA } from '@/data/dummyData'
 
 const CategoryIndexPage = () => {
-  const user = null;
+  // const user = null;
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    is_administrator: boolean;
+    is_super_admin: boolean;
+  };
+  
+  const user: User = {
+    id: "1", // Replace with appropriate default values
+    name: "Guest",
+    email: "guest@example.com",
+    is_administrator: false,
+    is_super_admin: false,
+  };
   
   return (
     <div className="flex h-full flex-col-reverse md:flex-row gap-[12px] mb-2 animate-fadeIn">
